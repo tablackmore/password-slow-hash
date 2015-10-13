b40e55b1524e709d2706f5ed24596b4d768d9d6a
Tom Blackmore <tablackmore@gmail.com>
2015-10-13 18:15:50 +0200

    added some mocha tests

---
+++ test/passwordHash-tst.js
@@ -0,0 +1,34 @@
+/*'use strict';
+var password = require('../password-hash');
+var test2 = 'some long random text #€%&/UHGNJTYU/&JUYI(K(/K(K(HSEGRFW DFGHDHB RGEGREGNABERHBS €E RGRW GTY THTRY TRH TRHTR HTR YTR YTRH%#%';
+var options = {
+	iterations: 3000,
+	algorithm: 'sha512',
+	saltByteSize: 256,
+	hashByteSize: 256,
+	returnType: 'object'
+};
+
+password.hash(test2, options, function (err, output) {
+	if (!err) {
+		console.log(output);
+
+		password.validate(test2, output, function (err, result) {
+			console.log(result);
+		});
+	} else {
+		console.error(err);
+	}
+});
+
+password.hash(test2, function (err, output) {
+	if (!err) {
+		console.log(output);
+
+		password.validate(test2, output, function (err, result) {
+			console.log(result);
+		});
+	} else {
+		console.error(err);
+	}
+});*/
